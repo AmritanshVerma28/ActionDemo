429de485c23dad979d95d28d576b7e1678a5c0c0
averm234 <amritansh.verma@aexp.com>
2023-06-04 00:04:24 +0530

    c3

--- customAdder.py
+++ customAdder.py
@@ -1,6 +1,7 @@
 class myAdder:
-
+    #print("class called")
     # my custom function to add 2 numbers
-    def add_2_numbers(a,b):
+    def add_2_numbers(self,a,b):
+        #print("func called")
         c = a + b
         return c
